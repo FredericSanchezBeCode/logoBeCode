afb65d1c64a4802be92156c0765d45b1c14fbb4b
massartval <massart.val@gmail.com>
2021-03-03 12:28:44 +0100

    commented out rotation in style.css

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,9 @@
 // Scripts go here
+$("#button").on("click", function () {
+  $("#parent").css({ transform: "rotate(120deg)" });
+  $("#child").css({ transform: "rotate(-120deg)" });
+});
+
+document.on("load", () => {
+  $;
+});
